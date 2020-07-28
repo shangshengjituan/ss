1720a5a14505178f62ff7021bb8afedd999e9779
FeiYangbuMian <320801223@qq.com>
2020-07-28 11:27:40 +0800

    发票、收款、合同价款已经写好

---
+++ caiwu/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <el-backtop></el-backtop>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">情况</el-menu-item>
+        <el-menu-item index="2">汇总</el-menu-item>
+        <el-menu-item index="3">常用工具</el-menu-item>
+      </el-menu>
+      <sheet v-if="activeIndex === '1'"/>
+      <summary-sheet v-if="activeIndex === '2'"/>
+      <!--<summary v-if="activeIndex === '2'"/>-->
+      <tool v-if="activeIndex === '3'"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Sheet from "./views/Sheet";
+import SummarySheet from "./views/SummarySheet";
+import Tool from "./views/Tool";
+export default {
+  name: 'App',
+	components: {Tool, SummarySheet, Sheet},
+	data () {
+		return {
+			activeIndex: '1'
+		}
+	},
+	methods: {
+		handleSelect (key, keyPath) {
+			this.activeIndex = keyPath[0]
+		}
+	}
+}
+</script>
+
+<style>
+  .container {
+    width: calc(100% - 20px);
+    margin: 10px 10px 60px;
+  }
+  .width-full {
+    width: 100% !important;
+  }
+</style>
